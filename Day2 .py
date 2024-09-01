9393757525199ffb373a06b98725085e55f5aebf
SORE188 <emsore7@gmail.com>
2024-09-01 16:09:14 +0300

    assignment done

---
+++ Day2 .py
@@ -0,0 +1,46 @@
+#Variables
+# FavouriteNumber = 2000000
+# print(f"My favourite number is :{FavouriteNumber}")
+
+# #string
+# greeting = "Hello, PLPL Academy!"
+# print(greeting)
+
+#Boolean
+# is_python_fun = True
+# is_python_boring = False
+# print(f"Is python fun? {is_python_fun}")
+# print (f"Is Python Boring? {is_python_boring}")
+
+#Control_Flow
+# temperature = 16
+# if temperature > 25:
+#     print("It's hot outside! Wear Shorts.")
+# elif temperature > 15:
+#     print("It's warm. Maybe wear a light jacket!")  
+# else:
+#     print("Brrr! It's cold. Bundle up!")   
+
+#Loops
+# for i in range(7):
+#     print(f"This is a car {i}")
+
+#     #while loop
+# countdown = 5
+# while countdown > 0
+#     print (f"Count down: {CountDown}")  
+#     countdown -= 1  
+# print("Blast Off")
+
+#Modules
+#In-built
+import math
+
+result = math.sqrt(16)
+print(f"The squuare root of 16 is:{result}")
+
+#Customized module
+import salamu
+
+print(salamu.greet("Chakin"))
+
---
+++ salamu.py
@@ -0,0 +1,6 @@
+#define the function
+def greet(somone):
+    return f"Good afternoon, {somone}"
+
+print(greet("Doreen"))
+print(greet("Fondo"))
